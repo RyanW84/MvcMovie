5f42d759b097fcaf6147ab47410545bdf30cd47e
Ryan Weavers <ryanweavers@gmail.com>
2026-04-12 20:24:06 +0100

    Scaffolderd correctly

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75db0a7e8f522cc019adc56fb0ec216bedcca826")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7ca4f62dbbab7a5db8061ca56d8d4dba611f8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
