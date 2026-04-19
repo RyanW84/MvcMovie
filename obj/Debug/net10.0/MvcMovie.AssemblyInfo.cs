6d8f6d0d7f939dbe891e6f4966485b551d3d86fc
Ryan Weavers <ryanweavers@gmail.com>
2026-04-19 16:01:11 +0100

    feat(DB): DB fully working now

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f42d759b097fcaf6147ab47410545bdf30cd47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e5248587671c74e84dcd5bb4c70ba9de066c85")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
