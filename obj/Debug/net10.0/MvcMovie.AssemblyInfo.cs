00b95d0f36bdf2771cf5d4a624616a61a66d7678
Ryan Weavers <ryanweavers@gmail.com>
2026-04-22 20:40:03 +0100

    Quick Push

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e5248587671c74e84dcd5bb4c70ba9de066c85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa801e2432756e66ea3a2f76a26e2a3f724080dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
